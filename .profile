e282c3533f075c3c5c472a4a1f27dbe93d685d09
akshit-sharma <akshit9sharma@gmail.com>
2018-06-12 10:48:40 -0600

    renamed .profile to .my_profile

--- .profile
+++
@@ -1,5 +0,0 @@
-# configuration file synced across device
-# don't make any system specific changes in this
-
-source $HOME/.my_vars
-
--- update.sh
+++ update.sh
@@ -4,18 +4,30 @@ if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 fi
 
+if cat ~/.bashrc | grep -xqFe "source ~/.my_profile" 
+then
+  echo "~/.bashrc already calling ~/.my_profile"
+else
+  echo "" >> ~/.bashrc
+  echo "source ~/.my_profile" >> ~/.bashrc
+  echo "" >> ~/.bashrc
+fi
+
+
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 if [ ! -L "$HOME/.vimrc" ]; then
   ln -sT $SCRIPTPATH/.vimrc $HOME/.vimrc
 fi
 
-if [ ! -L "$HOME/.profile" ]; then
-  ln -sT $SCRIPTPATH/.profile $HOME/.profile
+if [ ! -L "$HOME/.my_profile" ]; then
+  ln -sT $SCRIPTPATH/.my_profile $HOME/.my_profile
   if [ ! -f "$HOME/.my_vars" ]; then
     touch $HOME/.my_vars
   fi
-fi 
+fi
+
+mkdir -p ~/.vim/tags
 
 vim +PluginInstall +qall
 
