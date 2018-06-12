8b3631ad453bc5e6b39a17f6b78a867eb6b80cc4
akshit-sharma <akshit9sharma@gmail.com>
2018-06-12 11:01:15 -0600

    calling .my_profile for current session

--- update.sh
+++ update.sh
@@ -1,18 +1,9 @@
 # /bin/bash
 
+# plugin manager for vim
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 fi
-
-if cat ~/.bashrc | grep -xqFe "source ~/.my_profile" 
-then
-  echo "~/.bashrc already calling ~/.my_profile"
-else
-  echo "" >> ~/.bashrc
-  echo "source ~/.my_profile" >> ~/.bashrc
-  echo "" >> ~/.bashrc
-fi
-
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
@@ -28,6 +19,21 @@ if [ ! -L "$HOME/.my_profile" ]; then
 fi
 
 mkdir -p ~/.vim/tags
+
+# all symlink done (configuration structure established)
+
+if cat ~/.bashrc | grep -xqFe "source ~/.my_profile" 
+then
+  echo "~/.bashrc already calling ~/.my_profile"
+else
+  echo "" >> ~/.bashrc
+  echo "source ~/.my_profile" >> ~/.bashrc
+  echo "" >> ~/.bashrc
+
+  # call the ~/my_profile (it was not in ~/.bashrc in this session
+  source ~/.my_profile  
+
+fi
 
 vim +PluginInstall +qall
 
