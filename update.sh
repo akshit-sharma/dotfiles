426413502ad870fd2091f0833bfdbf73d755c60b
akshit-sharma <akshit9sharma@gmail.com>
2018-07-08 15:59:57 -0600

    made repo position dynamically though env var instead of hard coding

--- update.sh
+++ update.sh
@@ -1,12 +1,89 @@
 # /bin/bash
 
+NEED_VIM_PLUGIN_INSTALL=1
+NEED_BASH_REFRESH=0
+NEED_PROFILE_REFRESH=0
+
+DEBUG_SCRIPT=1
+
+# function for returning the parent directory of this script
+function parent_directory {
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    TARGET="$(readlink "$SOURCE")"
+    if [[ $TARGET == /* ]]; then
+      if [ DEBUG_SCRIPT -ne 0]; then
+        echo "SOURCE '$SOURCE' is an absolute symlink to '$TARGET'"
+      fi
+      SOURCE="$TARGET"
+    else
+      DIR="$( dirname "$SOURCE" )"
+      if [[ DEBUG_SCRIPT -ne 0 ]]; then
+        echo "SOURCE '$SOURCE' is a relative symlink to '$TARGET' (relative to '$DIR')"
+      fi
+      SOURCE="$DIR/$TARGET" # if $SOURCE was a relative symlink, 
+                            # we need to resolve it relative to the path where the symlink file was located
+    fi
+  done
+  if [[ DEBUG_SCRIPT -ne 0 ]]; then
+    echo "SOURCE is '$SOURCE'"
+  fi
+  RDIR="$( dirname "$SOURCE" )"
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  if [ "$DIR" != "$RDIR" ] && [[ DEBUG_SCRIPT -ne 0 ]]; then
+    echo "DIR '$RDIR' resolves to '$DIR'"
+  fi
+  if [[ DEBUG_SCRIPT -ne 0 ]]; then
+    echo "DIR is '$DIR'"
+  fi
+  SCRIPT_PARENT_DIRECTORY=$DIR
+}
+
+# function for adding lines to ~/.bashrc if not present
+# 1: LINE to change to if not present
+# 2: Initial value of Refresh
+# 3: Debug msg to print if present
+# 4: CMD to achive desired LINE (only if updating LINE instead of adding LINE) 
+function update_bashrc {
+  LINE=$1
+  REFRESH=$2
+  MSG_IF_PRESENT="${3:-3rd paramater to update_bashrc not given, no debug stmt}"
+  CMD="${4:-$LINE}"
+
+  if cat ~/.bashrc | grep -xqFe "$LINE"
+  then
+    if [[ DEBUG_SCRIPT -ne 0 ]]; then
+      echo "~/.bashrc $MSG_IF_PRESENT"
+    fi
+  else
+    if [ "$LINE" = "$CMD" ]; then 
+      echo "" >> ~/.bashrc
+      echo "$LINE" >> ~/.bashrc
+    else 
+      eval $CMD
+    fi
+    REFRESH=1
+  fi
+}
+
+if [ ! "$DOTFILES_SCRIPT_PARENT" ]; then
+  parent_directory
+  if [[ DEBUG_SCRIPT -ne 0 ]]; then
+    echo "Found parent directory as $SCRIPT_PARENT_DIRECTORY"
+  fi
+  SCRIPTPATH=$SCRIPT_PARENT_DIRECTORY
+else
+  if [[ DEBUG_SCRIPT -ne 0 ]]; then
+    echo "DOTFILES_SCRIPT_PARENT already set to $DOTFILES_SCRIPT_PARENT"
+  fi
+  SCRIPTPATH=$DOTFILES_SCRIPT_PARENT
+fi
+
 if [ -z "$1" ]; then
   NEED_PROFILE_REFRESH=1
 else
   NEED_PROFILE_REFRESH=0
 fi
-
-NEED_VIM_PLUGIN_INSTALL=1
 
 # if called by other script
 if [ $SHLVL -gt 2 ]; then  
@@ -15,17 +92,52 @@ if [ $SHLVL -gt 2 ]; then
   else
     NEED_VIM_PLUGIN_INSTALL=0
   fi
-fi 
+fi
+
+# function for creating symlink to files in $HOME/.config
+function home_config_symlink {
+  filename="$1"
+  if [[ DEBUG_SCRIPT -ne 0 ]]; then
+    echo "value is $SCRIPTPATH/$filename"
+  fi
+  return
+  if [ ! -f $SCRIPTPATH/$filename ]; then              # pretty much use less after getting files
+    cp $HOME/.config/$filename $SCRIPTPATH/$filename   # usefull to add new files in the script
+  fi                                                   # instead of manually moving/copying
+  if [ -f "$HOME/.config/$filename" ]; then
+    mv "$HOME/.config/$filename" "$HOME/.config/$filename.bk"
+  fi
+  if [ ! -L "$HOME/.config/$filename" ]; then
+    ln -sT $SCRIPTPATH/$filename $HOME/.config/$filename
+  fi
+}
+
+
+# if kde plasma get my shortcuts
+if [ "$DESKTOP_SESSION" = "plasma" ]; then
+  if [[ DEBUG_SCRIPT -ne 0 ]]; then
+     echo "plasma (kde) detected"
+  fi
+  PLASMASHELL_VERSION=`eval plasmashell --version | sed -nr 's/plasmashell ([0-9][0-9]*\.*)/\1/p'`
+  PLASMASHELL_MAJOR=`plasmashell --version | sed -rn 's/plasmashell ([0-9])\.[0-9].*/\1/p'`
+  if [ $PLASMASHELL_MAJOR -eq 5 ]; then
+    home_config_symlink kglobalshortcutsrc
+    home_config_symlink khotkeysrc
+    home_config_symlink quicktile.cfg
+  else
+    echo "Don't know how to handle this plasma version"
+    echo "PLASMASHELL_VERSION $PLASMASHELL_VERSION"
+    echo "PLASMASHELL_MAJOR $PLASMASHELL_VERSION"
+    OUTPUT_PLASMASHELL=`eval plasmashell --version`
+    echo "plasmashell --version is $OUTPUT_PLASMASHELL"
+  fi
+fi
 
 # plugin manager for vim
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
   NEED_VIM_PLUGIN_INSTALL=1
 fi
-
-NEED_BASH_REFRESH=0
-
-SCRIPTPATH=$HOME/dotfiles
 
 if [ ! -L "$HOME/.vimrc" ]; then
   ln -sT $SCRIPTPATH/.vimrc $HOME/.vimrc
@@ -46,34 +158,46 @@ mkdir -p ~/.vim/tags
 
 # all symlink done (configuration structure established)
 
-if cat ~/.bashrc | grep -xqFe "source ~/.my_profile" 
-then
-  echo "~/.bashrc already calling ~/.my_profile"
-else
-  echo "" >> ~/.bashrc
-  echo "source ~/.my_profile" >> ~/.bashrc
-  echo "" >> ~/.bashrc
-  NEED_PROFILE_REFRESH=1
-fi
+update_bashrc "^force_color_prompt=yes" $NEED_BASH_REFRESH "force_color_prompt already set" \
+              "sed -i 's/#force_color_prompt/force_color_prompt/g' ~/.bashrc"
+NEED_BASH_REFRESH=$REFRESH
 
-if cat ~/.bashrc | grep -xqFe "#force_color_prompt=yes"
-then
-  sed -i 's/#force_color_prompt/force_color_prompt/g' ~/.bashrc
-  NEED_BASH_REFRESH=1
-else
-  echo "force_color_prompt already set"
-fi
+update_bashrc DOTFILES_SCRIPT_PARENT=$SCRIPTPATH $NEED_BASH_REFRESH "DOTFILES_SCRIPT_PARENT env var set"
+NEED_BASH_REFRESH=$REFRESH
 
-if [ "$NEED_BASH_REFRESH" ]; then
-  source ~/.bashrc
+update_bashrc "export DOTFILES_SCRIPT_PARENT" $NEED_BASH_REFRESH "DOTFILES_SCRIPT_PARENT env already exported"
+NEED_BASH_REFRESH=$REFRESH
+
+update_bashrc "source ~/.my_profile" $NEED_PROFILE_REFRESH "already calling ~/.my_profile" 
+NEED_PROFILE_REFRESH=$REFRESH
+
+
+  echo "bef val of bash and prof refresh are $NEED_BASH_REFRESH and $NEED_PROFILE_REFRESH"
+
+if [[ NEED_BASH_REFRESH -ne 0 ]]; then
+  if [[ DEBUG_SCRIPT -ne 0 ]]; then
+     echo "sourcing bashrc"
+  fi
+  #source ~/.bashrc # cannot source bashrc from script
+  source ~/.my_profile "$SCRIPTPATH"
   NEED_PROFILE_REFRESH=0
 fi
 
-if [ "$NEED_PROFILE_REFRESH" ]; then
-  source ~/.my_profile
+echo "after bash of bash and prof refresh are $NEED_BASH_REFRESH and $NEED_PROFILE_REFRESH"
+
+if [[ NEED_PROFILE_REFRESH -ne 0 ]]; then
+  if [[ DEBUG_SCRIPT -ne 0 ]]; then
+     echo "sourcing my_profile $SCRIPTPATH"
+  fi
+  source ~/.my_profile "$SCRIPTPATH"
 fi
 
-if [ "$NEED_VIM_PLUGIN_INSTALL" ]; then
+echo "after prof val of bash and prof refresh are $NEED_BASH_REFRESH and $NEED_PROFILE_REFRESH"
+
+if [[ NEED_VIM_PLUGIN_INSTALL -ne 0 ]]; then
+  if [[ DEBUG_SCRIPT -ne 0 ]]; then
+     echo "running vim +PluginInstall...."
+  fi
   vim +PluginInstall +qall
 fi
 
