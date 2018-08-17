350b970ba6f6bc1b08e3e633ff2b6859058a5010
akshit-sharma <akshit9sharma@gmail.com>
2018-08-16 21:27:15 -0600

    modified to work with clean install

---
+++ download_and_install.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# Make github key and install dotfiles in $HOME with having git ssh configurated
+
+
+file_name=${HOME}/.ssh/id_${HOSTNAME}_github
+
+i="1"
+if [ ! -f $file_name ]; then
+  echo "Enter email id :"
+  read email_id
+
+  ssh-keygen -f $file_name -t rsa -C ${email_id}
+
+  echo "Copy the ${file_name}.pub to Github"
+  echo "Press any key to continue"
+  read something
+fi
+
+echo "testing connection"
+ssh -T git@github.com -i ${file_name}
+    
+if [ "$?" == "1" ]; then
+  i="0"
+fi
+
+echo "value of i is $i"
+if [ "$i" != "0" ]; then 
+  while [ $i -ne 0 ]; do
+    echo "Copy the ${file_name}.pub to Github"
+    echo "Press any key to continue"
+    read something
+
+    echo "testing connection"
+    ssh -T git@github.com -i ${file_name}
+
+    if [ "$?" != "1" ]; then
+      echo "Connection to github not setup yet"
+      i="1"
+    else
+      i="0"
+    fi
+  done
+
+  echo "Enter github username:"
+  read username
+
+  if [ ! -f $HOME/.ssh/config ]; then
+    touch $HOME/.ssh/config
+  fi
+
+  echo "" >> $HOME/.ssh/config
+  echo "host github.com" >> $HOME/.ssh/config
+  echo "  User ${username}" >> $HOME/.ssh/config
+  echo "  HostName github.com" >> $HOME/.ssh/config
+  echo "  IdentityFile ~/.ssh/id_${HOSTNAME}_github" >> $HOME/.ssh/config
+  echo "" >> $HOME/.ssh/config
+
+fi
+
+if [ ! -d $HOME/dotfiles ]; then
+  git --version
+  if [ "$?" != "0"]; then
+    echo "git not installed"
+  else
+    git clone git@github.com:akshit-sharma/dotfiles.git $HOME/dotfiles
+  fi
+fi
+
+source $HOME/dotfiles/install.sh
+
