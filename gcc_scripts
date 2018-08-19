2ddbd46760f81e5817022e1ad51a1fced818f619
akshit-sharma <akshit9sharma@gmail.com>
2018-08-19 12:03:43 -0600

    Added script for installing gcc 7.3

---
+++ gcc_scripts
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+
+PATH=$HOME/Softwares/gcc/bin:$PATH
+LD_LIBRARY_PATH=$HOME/Softwares/gcc/lib64:$LD_LIBRARY_PATH
+
+export PATH
+export LD_LIBRARY_PATH
+
---
+++ primary.conf/gcc.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+#
+
+GCC_SCRIPT_PATH=""
+function parent_dir {
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was
+          located
+  done
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+  GCC_SCRIPT_PATH=$DIR
+}
+
+if [ ! -f $GCC_SCRIPT_PATH/../faaltu/gcc.done ]; then
+
+  parent_dir
+
+  GCC_SRC_HOME=$HOME/Libraries/gcc-from-src
+  GCC_BIN_HOME=$HOME/Softwares/gcc
+
+  GCC_CORRECT_MD5="747d5010b7c6938b480bc6e4d7c4be9a"
+  GCC_VERSION="7.3"
+  GCC_TAR_DIR="gcc-$GCC_VERSION.0"
+  GCC_TAR_GZ="$GCC_TAR_DIR.tar.gz"
+  GCC_TAR_URL="https://ftpmirror.gnu.org/gcc/$GCC_TAR_DIR/$GCC_TAR_GZ"
+
+  if [ ! -d $GCC_SRC_HOME ]; then
+    mkdir -p $GCC_SRC_HOME
+  fi
+  if [ ! -d  "$GCC_SRC_HOME/$GCC_TAR_DIR" ]; then
+    if [ ! -f "$GCC_SCRIPT_PATH/../faaltu/$GCC_TAR_GZ" ]; then
+      echo "downloading $GCC_SCRIPT_PATH/../$GCC_TAR_GZ"
+      wget $GCC_TAR_URL -O $GCC_SCRIPT_PATH/../faaltu/$GCC_TAR_GZ
+    else
+      GCC_DOWNLOAD_MD5=`eval md5sum $GCC_SCRIPT_PATH/../faaltu/$GCC_TAR_GZ | cut -d' ' -f1`
+      if [ "$GCC_CORRECT_MD5" != "$GCC_DOWNLOAD_MD5" ]; then
+        echo "redownloading gcc $GCC_SCRIPT_PATH/../faaltu/$GCC_TAR_GZ"
+        rm $GCC_SCRIPT_PATH/../faaltu/$GCC_TAR_GZ
+        wget $GCC_TAR_URL -O $GCC_SCRIPT_PATH/../faaltu/$GCC_TAR_GZ
+      fi
+    fi
+    tar -xf $GCC_SCRIPT_PATH/../faaltu/$GCC_TAR_GZ -C $GCC_SRC_HOME
+  else
+    GCC_DOWNLOAD_MD5=`eval md5sum $GCC_SCRIPT_PATH/../faaltu/$GCC_TAR_GZ | cut -d' ' -f1`
+    if [ "$GCC_CORRECT_MD5" != "$GCC_DOWNLOAD_MD5" ]; then
+      echo "downloading new version of gcc"
+      rm $GCC_SCRIPT_PATH/../faaltu/$GCC_TAR_GZ
+      wget $GCC_TAR_URL -O $GCC_SCRIPT_PATH/../faaltu/$GCC_TAR_GZ
+      if [ -d $GCC_SRC_HOME/$GCC_TAR_DIR ]; then
+        rm -rf $GCC_SRC_HOME/$GCC_TAR_DIR
+      fi
+      if [ -f "$GCC_SCRIPT_PATH/../faaltu/.gcc-prerequisites.md5" ]; then
+        rm $GCC_SCRIPT_PATH/../faaltu/.gcc-prerequisites.md5
+      fi
+      tar -xf $GCC_SCRIPT_PATH/../faaltu/$GCC_TAR_GZ -C $GCC_SRC_HOME
+    else
+      echo "$GCC_SCRIPT_PATH/../faaltu/$GCC_TAR_GZ up-to-date"
+    fi
+  fi
+  if [ ! -f "$GCC_SCRIPT_PATH/../faaltu/.gcc-prerequisites.md5" ]; then
+    cd $GCC_SRC_HOME/$GCC_TAR_DIR
+    ./contrib/download_prerequisites
+    if [ "$?" != 0 ]; then
+      echo "Error downloading prerequisites for gcc"
+      exit 1 
+    fi
+    GCC_DOWNLOAD_PREREQUISITES_MD5=`eval md5sum contrib/download_prerequisites | cut -d' ' -f1`
+    echo $GCC_DOWNLOAD_PREREQUISITES_MD5 > $GCC_SCRIPT_PATH/../faaltu/.gcc-prerequisites.md5 
+    if [ -f "$GCC_SCRIPT_PATH/../faaltu/.gcc-build" ]; then
+      rm $GCC_SCRIPT_PATH/../faaltu/.gcc-build
+    fi
+    cd $GCC_SCRIPT_PATH
+  fi
+  if [ ! -f "$GCC_SCRIPT_PATH/../faaltu/.gcc-build" ]; then
+    cd $GCC_SRC_HOME/
+    if [ ! -d build ]; then
+      mkdir build
+    fi
+    cd build
+    ../$GCC_TAR_DIR/configure -v --build=x86_64-linux-gnu --host=x86_64-linux-gnu \
+                  target=x86_64-linux-gnu --prefix=$GCC_BIN_HOME --enable-checking=release \
+                  --enable-languages=c,c++ --disable-multilib --program-suffix=-$GCC_VERSION
+    for ((i=16; i >= 1; i = i/2))
+    do
+      echo "Running with $i jobs"
+      make -j $i
+      if [ $? -ne 0 ] && [ $i -eq 1 ]; then
+        echo "Error in running with 1 job"
+        echo "Returning"
+        exit 2
+      fi
+    done 
+    make install
+    cd ..
+    rm -rf build # remove build intermediate results
+    touch $GCC_SCRIPT_PATH/../faaltu/.gcc-build
+    cd $GCC_SCRIPT_PATH
+  fi
+
+  touch $GCC_SCRIPT_PATH/../faaltu/gcc.done 
+
+fi
