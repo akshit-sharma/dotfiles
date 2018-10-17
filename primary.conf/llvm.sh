22950534e727507ef37d32ea77157c6ebb1a8951
akshit-sharma <akshit9sharma@gmail.com>
2018-10-17 01:30:50 -0400

    llvm make taking number of cores and system memory into consideration for building

--- primary.conf/llvm.sh
+++ primary.conf/llvm.sh
@@ -46,9 +46,12 @@ if [ ! -f $LLVM_SCRIPT_PATH/../faaltu/llvm.done ]; then
   doxygen --version
   DOXYGEN_RET=$?
 
-  if [[ $SVN_RET -ne 0 ]] || [[ $MAKE_RET -ne 0 ]] || [[ $GCC_RET -ne 0 ]] || [[ $PYTHON_RET -ne 0 ]] || [[ $DOXYGEN_RET -ne 0 ]]; then
+  sphinx --version
+  SPHINX_RET=$?
+
+  if [[ $SVN_RET -ne 0 ]] || [[ $MAKE_RET -ne 0 ]] || [[ $GCC_RET -ne 0 ]] || [[ $PYTHON_RET -ne 0 ]] || [[ $DOXYGEN_RET -ne 0 ]] || [[ $SPHINX_RET -ne 0 ]]; then
     CMD="sudo apt-get install -y "
-    CMD="sudo apt-get install -y subversion make gcc python doxygen"
+    # CMD="sudo apt-get install -y subversion make gcc python doxygen"
     if [[ $SVN_RET -ne 0 ]]; then
       CMD="$CMD subversion"
     fi
@@ -63,6 +66,10 @@ if [ ! -f $LLVM_SCRIPT_PATH/../faaltu/llvm.done ]; then
     fi
     if [[ $DOXYGEN_RET -ne 0 ]]; then
       CMD="$CMD doxygen"
+    fi
+    if [[ $SPHINX_RET -ne 0 ]]; then
+      pip install -U sphinx
+      pip3 install -U sphinx
     fi
     echo $CMD
     $CMD
@@ -97,15 +104,18 @@ if [ ! -f $LLVM_SCRIPT_PATH/../faaltu/llvm.done ]; then
   if [ ! -d $LLVM_BIN_HOME ]; then
     mkdir -p $LLVM_BIN_HOME
   fi 
+  
+  SYSTEM_CORES=`grep -c ^processor /proc/cpuinfo`
+  SYSTEM_MEMORY=`grep MemTotal /proc/meminfo | awk '{print $2}'`
 
   if [ ! -d $LLVM_SRC_HOME/build-release ]; then
     mkdir $LLVM_SRC_HOME/build-release
     cd $LLVM_SRC_HOME/build-release 
     cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release \
               -DCMAKE_INSTALL_PREFIX=$LLVM_BIN_HOME/llvm ../llvm \
-              -DLLVM_ENABLE_DOXYGEN=true
-              # -DLLVM_ENABLE_SPHINX=true \
-              # -DSPHINX_OUTPUT_HTML=true
+              -DLLVM_ENABLE_SPHINX=true \
+              -DSPHINX_OUTPUT_HTML=true
+              # -DLLVM_ENABLE_DOXYGEN=true
               # -DCMAKE_C_COMPILER=$LLVM_SCRIPT_PATH/../faaltu/clang+llvm/bin/clang \
               # -DCMAKE_CXX_COMPILER=$LLVM_SCRIPT_PATH/../faaltu/clang+llvm/bin/clang++ \
               # -DLLVM_BUILD_LLVM_DYLIB=ON                                              \
@@ -119,20 +129,23 @@ if [ ! -f $LLVM_SCRIPT_PATH/../faaltu/llvm.done ]; then
       exit 5
     fi
   fi
-  for ((i=8; i >= 1; i = i/2))
+  SYSTEM_JOBS_ACCORDING_TO_MEMORY=$((SYSTEM_MEMORY / 2000000 ))
+  SYSTEM_JOBS=$(($SYSTEM_JOBS_ACCORDING_TO_MEMORY>$SYSTEM_CORES?$SYSTEM_CORES:$SYSTEM_JOBS_ACCORDING_TO_MEMORY))
+  for ((i=$SYSTEM_JOBS; i >= 1; i = i/2))
   do
     echo "Running with $i jobs"
     make -j $i
-    if [ $? -eq 0 ]; then
+    MAKE_RET=$?
+    if [ $MAKE_RET -eq 0 ]; then
       echo "make successful with $i jobs"
       break 
     fi
-    if [ $? -ne 0 ] && [ $i -eq 1 ]; then
-      echo "Error in running with 1 job"
-      echo "Returning"
-      exit 2
-    fi
   done
+  if [ $MAKE_RET -ne 0 ]; then
+    echo "Error in running with 1 job"
+    echo "Returning"
+    exit 2
+  fi
   make install
   MAKE_INSTALL_RET="$?"
   if [ $MAKE_INSTALL_RET -ne 0 ]; then
@@ -145,9 +158,9 @@ if [ ! -f $LLVM_SCRIPT_PATH/../faaltu/llvm.done ]; then
     cd $LLVM_SRC_HOME/build-debug
     cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug \
               -DCMAKE_INSTALL_PREFIX=$LLVM_BIN_HOME/llvm-dbg  ../llvm \
-              -DLLVM_ENABLE_DOXYGEN=true
-              # -DLLVM_ENABLE_SPHINX=true \
-              # -DSPHINX_OUTPUT_HTML=true
+              -DLLVM_ENABLE_SPHINX=true \
+              -DSPHINX_OUTPUT_HTML=true
+              # -DLLVM_ENABLE_DOXYGEN=true
               # -DCMAKE_C_COMPILER=$LLVM_SCRIPT_PATH/../faaltu/clang+llvm/bin/clang \
               # -DCMAKE_CXX_COMPILER=$LLVM_SCRIPT_PATH/../faaltu/clang+llvm/bin/clang++ 
     CMAKE_RET="$?"
@@ -159,20 +172,23 @@ if [ ! -f $LLVM_SCRIPT_PATH/../faaltu/llvm.done ]; then
       exit 5
     fi
   fi
-  for ((i=8; i >= 1; i = i/2))
+  SYSTEM_JOBS_ACCORDING_TO_MEMORY=$((SYSTEM_MEMORY / 8000000 ))
+  SYSTEM_JOBS=$(($SYSTEM_JOBS_ACCORDING_TO_MEMORY>$SYSTEM_CORES?$SYSTEM_CORES:$SYSTEM_JOBS_ACCORDING_TO_MEMORY))
+  for ((i=$SYSTEM_JOBS; i >= 1; i = i/2))
   do
     echo "Running with $i jobs"
     make -j $i
-    if [ $? -eq 0 ]; then
+    MAKE_RET=$?
+    if [ $MAKE_RET -eq 0 ]; then
       echo "make successful with $i jobs"
       break 
     fi
-    if [ $? -ne 0 ] && [ $i -eq 1 ]; then
-      echo "Error in running with 1 job"
-      echo "Returning"
-      exit 2
-    fi
   done
+  if [ $MAKE_RET -ne 0 ]; then
+    echo "Error in running with 1 job"
+    echo "Returning"
+    exit 2
+  fi
   make install
   MAKE_INSTALL_RET="$?"
   if [ $MAKE_INSTALL_RET -ne 0 ]; then
