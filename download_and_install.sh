8a3c594691f6fa216f1d1df2fb33453ed546b7e5
Akshit Sharma <akshit9sharma@gmail.com>
2018-12-18 21:55:36 -0700

    Update download_and_install.sh
    SSH_OFF is used from environment

--- download_and_install.sh
+++ download_and_install.sh
@@ -4,7 +4,7 @@
 
 DEFAULTPATH=${HOME}/dotfiles
 file_name=${HOME}/.ssh/id_${HOSTNAME}_github
-SSH_OFF="1"
+SSH_OFF="${SSH_OFF:-1}"
 
 if [ -z ${DOTFILES_SCRIPT_DIR} ]; then
   SCRIPT_PATH=$DEFAULTPATH
