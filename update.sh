8a6121607e314a4a6e62a4949dbd0e292f795fca
akshit-sharma <akshit9sharma@gmail.com>
2020-04-25 01:21:07 -0600

    added more verbose for download and extract

--- update.sh
+++ update.sh
@@ -223,6 +223,7 @@ function download_and_extract {
 
     if [ ! -f $DOWNLOAD_HOME/$DOWNLOAD_FILE ]; then
       echo "downloading $DOWNLOAD_FILE to $DOWNLOAD_HOME"
+      echo "wget $DOWNLOAD_URL -q -O $DOWNLOAD_HOME/$DOWNLOAD_FILE"
       wget $DOWNLOAD_URL -q -O $DOWNLOAD_HOME/$DOWNLOAD_FILE 
       WGET_RETURN="$?"
       if [ $WGET_RETURN -ne "0" ]; then
@@ -230,7 +231,7 @@ function download_and_extract {
       fi
     fi
 
-      EXISTING_MD5=`eval md5sum $DOWNLOAD_HOME/$DOWNLOAD_FILE | cut -d' ' -f1`
+    EXISTING_MD5=`eval md5sum $DOWNLOAD_HOME/$DOWNLOAD_FILE | cut -d' ' -f1`
     if [ ! -f $DOWNLOAD_HOME/$DOWNLOAD_FILE ] || [ "$DOWNLOAD_MD5" != "$EXISTING_MD5" ]; then
       echo "md5sum still not same expected(${DOWNLOAD_MD5}) and got(${EXISTING_MD5})"
       echo "file in ${DOWNLOAD_HOME}/${DOWNLOAD_FILE} not as expected"
@@ -246,12 +247,15 @@ function download_and_extract {
       return
     fi
     if [[ $DOWNLOAD_HOME/$DOWNLOAD_FILE == *.xz ]]; then
+      echo "extracing (tar -xf $DOWNLOAD_HOME/$DOWNLOAD_FILE -C $DOWNLOAD_HOME)"
       tar -xf $DOWNLOAD_HOME/$DOWNLOAD_FILE -C $DOWNLOAD_HOME
       SCRIPT_SUCC=1
     elif [[ $DOWNLOAD_HOME/$DOWNLOAD_FILE == *.gz ]]; then
+      echo "extracting (tar -zxf $DOWNLOAD_HOME/$DOWNLOAD_FILE -C $DOWNLOAD_HOME)"
       tar -zxf $DOWNLOAD_HOME/$DOWNLOAD_FILE -C $DOWNLOAD_HOME
       SCRIPT_SUCC=1
     elif [[ $DOWNLOAD_HOME/$DOWNLOAD_FILE == *bz2 ]]; then
+      echo "extracting (tar -xjf $DOWNLOAD_HOME/$DOWNLOAD_FILE -C $DOWNLOAD_HOME)"
       tar -xjf $DOWNLOAD_HOME/$DOWNLOAD_FILE -C $DOWNLOAD_HOME
       SCRIPT_SUCC=1
     elif [[ $DOWNLOAD_FILE == *.sh ]]; then
