f3f37bdd36e4d36b45ed7871735530c01c349220
akshit-sharma <akshitsharma@mymail.mines.edu>
2021-03-18 03:36:37 +0000

    updated sketelon.cmake, copying database compilation not needed anymore

--- templates/skeleton.cmake
+++ templates/skeleton.cmake
@@ -4,25 +4,16 @@ cmake_minimum_required(VERSION 3.14)
 project()
 
 set(CONAN_SYSTEM_INCLUDES "On")
-if (NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/conan.cmake")
-  message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/conan.cmake")
+  message(STATUS "Downloading conan.cmake from \
+          https://github.com/conan-io/cmake-conan")
   file (DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/master/conan.cmake"
-                 "${CMAKE_CURRENT_BINARY_DIR}/conan.cmake")
+    "${CMAKE_CURRENT_BINARY_DIR}/conan.cmake")
 endif()
-include ("${CMAKE_CURRENT_BINARY_DIR}/conan.cmake")
+include("${CMAKE_CURRENT_BINARY_DIR}/conan.cmake")
 conan_cmake_run(
   CONANFILE conanfile.txt
   BASIC_SETUP
   BUILD missing)
 
 add_subdirectory(src)
-
-add_custom_target(copy_compile_commands
-  DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/compile_commands.json)
-add_custom_command(
-  TARGET copy_compile_commands
-  PRE_BUILD
-  COMMAND ${CMAKE_COMMAND} -E copy_if_different
-   ${CMAKE_CURRENT_BINARY_DIR}/compile_commands.json
-   ${CMAKE_CURRENT_SOURCE_DIR}/compile_commands.json
-  )
