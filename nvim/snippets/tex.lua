9bec67e9278e35107417bda5db87a489ae3168e5
akshit-sharma <akshit9sharma@gmail.com>
2023-05-05 17:42:41 -0600

    updated snippets folder

---
+++ nvim/snippets/tex.lua
@@ -0,0 +1,32 @@
+return {
+  s({trig="tt", dscr="Expands 'tt' to '\\texttt{}'"},
+    {t("\\texttt{"), i(1), t("}")}
+  ),
+  s({trig=';a', snippetType="autosnippet"},
+    {
+      t('\\alpha'),
+    }
+  ),
+  s({trig=';b', snippetType="autosnippet"},
+    {
+      t('\\beta'),
+    }
+  ),
+  s({trig=';g', snippetType="autosnippet"},
+    {
+      t('\\gamma'),
+    }
+  ),
+  s({trig=';m', snippetType="autosnippet"},
+    {
+      t('\\mu'),
+    }
+  ),
+  s({trig=';n', snippetType="autosnippet"},
+    {
+      t('\\nu'),
+    }
+  ),
+
+}
+
