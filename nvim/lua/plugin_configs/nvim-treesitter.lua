feac973642b972794567806ff02285c44644552f
akshit-sharma <akshit9sharma@gmail.com>
2023-05-15 20:49:00 -0600

    fixed set query maybe

--- nvim/lua/plugin_configs/nvim-treesitter.lua
+++ nvim/lua/plugin_configs/nvim-treesitter.lua
@@ -119,8 +119,9 @@ if parsers.has_parser "c" and parsers.has_parser "cpp" and parsers.has_parser "c
   if not ok_query then
     vim.notify('query null, might want to fix', vim.log.levels.ERROR)
   else
-    query.set_query("c", "folds", folds_query)
-    query.set_query("cpp", "folds", folds_query)
-    query.set_query("cuda", "folds", folds_query)
+    local set_query = query.set or query.set_query
+    set_query("c", "folds", folds_query)
+    set_query("cpp", "folds", folds_query)
+    set_query("cuda", "folds", folds_query)
   end
 end
